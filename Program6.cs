e66a61cc67b1f9e4cd1836402185fd71613ddb52
smelikeaslan <aslansumeyramelike@gmail.com>
2024-10-20 23:42:55 +0300

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Birinci kenar uzunluğunu giriniz:");
+int birincikenar = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("İkinci kenar uzunluğunu giriniz:");
+int ikincikenar = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Üçüncü kenar uzunluğunu giriniz:");
+int ucuncukenar = Convert.ToInt32(Console.ReadLine());
+
+int hacim = birincikenar * ikincikenar * ucuncukenar;
+
+
+Console.Write("Dikdörtgenler prizmasının hacmi:" + hacim);
+
+Console.ReadKey();
